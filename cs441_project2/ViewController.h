a2ce31879f508b1ab9aa1a9d4a0f410b38139602
Ahmed Ihsan Erdem <ahmedihsanerdem@gmail.com>
2019-02-13 23:20:27 -0500

    Updated ViewController
    Signed-off-by: Ahmed Ihsan Erdem <ahmedihsanerdem@gmail.com>

--- cs441_project2/ViewController.h
+++ cs441_project2/ViewController.h
@@ -2,14 +2,25 @@
 //  ViewController.h
 //  cs441_project2
 //
-//  Created by Cihangir Umithan Goktolga on 12.02.2019.
+//  Created by Ahmed Ihsan Erdem on 12.02.2019.
 //  Copyright © 2019 Ahmed Ihsan Erdem. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import "Table.h"
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet Table *table;
+@property (strong, nonatomic) IBOutlet UISwipeGestureRecognizer *upSwipe;
+@property (strong, nonatomic) IBOutlet UISwipeGestureRecognizer *rightSwipe;
+@property (strong, nonatomic) IBOutlet UISwipeGestureRecognizer *downSwipe;
+@property (strong, nonatomic) IBOutlet UISwipeGestureRecognizer *leftSwipe;
+
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (weak, nonatomic) IBOutlet UILabel *highScoreLabel;
+@property (weak, nonatomic) IBOutlet UILabel *gameButton;
+
+- (void)updateScore;
 
 @end
-
--- cs441_project2/ViewController.m
+++ cs441_project2/ViewController.m
@@ -2,7 +2,7 @@
 //  ViewController.m
 //  cs441_project2
 //
-//  Created by Cihangir Umithan Goktolga on 12.02.2019.
+//  Created by Ahmed Ihsan Erdem on 12.02.2019.
 //  Copyright © 2019 Ahmed Ihsan Erdem. All rights reserved.
 //
 
